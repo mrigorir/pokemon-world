82c426d6ff9f6ae5d3889e297795ea537cd508ab
Mihndim Julius <mihndim2016@gmail.com>
2021-08-04 14:04:42 -0400

    Add functions to read and like number of likes

--- src/getItems.js
+++ src/getItems.js
@@ -1,5 +1,6 @@
 const pokeContainer = document.getElementById('pokeContainer');
 const pokemonNumber = 150;
+const appId = 'mig5MtTLHwlt6LzMMgLA';
 
 const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
@@ -8,3 +9,29 @@ const getPokemon = async (id) => {
   console.log(pokemon);
 }
 getPokemon(1);
+
+const upDateLikes = async (id) => {
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes/`
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      "item_id": id,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },   
+    
+  })
+  console.log(await response.text());
+}
+
+const getLikes = async (id) => {
+  const urlAll = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes/`
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes?item_id=${id}`
+  const response = await fetch(url)
+  const likes = await response.json();
+  let results = await likes.filter(like => like.item_id === id);
+  console.log(await results[0].likes);
+  // return likes;
+  // console.log(await response.json());
+}
