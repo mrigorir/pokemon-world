e19e1d7c1bf4430fd8a7741b399e46e348a1b23e
Mihndim Julius <mihndim2016@gmail.com>
2021-08-06 13:48:45 -0400

    Add template file

---
+++ src/tests/commentsTemplate.js
@@ -0,0 +1,20 @@
+const commentsTemplate = 
+`<div id="modalCommentsProfile">
+  <p class="comment"></p>
+  <p class="comment"></p>
+  <p class="comment"></p>
+  <p class="comment"></p>
+</div>`;
+export default commentsTemplate;
+1:37
+templateHTML.js
+1:37
+const template = `<div id="pokeList">
+<div class="card"></div>
+<div class="card"></div>
+<div class="card"></div>
+<div class="card"></div>
+<div class="card"></div>
+<div class="card"></div>
+</div>`;
+export default template;
